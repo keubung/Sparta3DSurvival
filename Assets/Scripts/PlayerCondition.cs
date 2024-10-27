f0305eb90f1debe45c4cabb1f5a5db4d238b67d0
keubung <skdud3390@naver.com>
2024-10-28 02:21:52 +0900

    [Add] 1-4
    Player UI (Health, Hunger, Stamina)

---
+++ Assets/Scripts/PlayerCondition.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCondition : MonoBehaviour
+{
+    public UICondition uICondition;
+
+    Condition health { get { return uICondition.health; } }
+    Condition hunger { get { return uICondition.hunger; } }
+    Condition stamina { get { return uICondition.stamina; } }
+
+    public float noHungerHealthDecay;
+
+    void Update()
+    {
+        hunger.Subtract(hunger.passiveValue * Time.deltaTime);
+        stamina.Add(stamina.passiveValue * Time.deltaTime);
+
+        if(hunger.curValue == 0f)
+        {
+            health.Subtract(noHungerHealthDecay * Time.deltaTime);
+        }
+
+        if(health.curValue == 0f)
+        {
+            Die();
+        }
+    }
+
+    public void Heal(float amout)
+    {
+        health.Add(amout);
+    }
+
+    public void Eat(float amout)
+    {
+        hunger.Add(amout);
+    }
+
+    public void Die()
+    {
+        Debug.Log("Die!");
+    }
+}
---
+++ Assets/Scripts/UI/DamageIndicator.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class DamagaIndicator : MonoBehaviour
+{
+
+}
